c9e3df5bab9cc1e5076219488a9a69423d9e608b
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-06-17 16:10:06 +0900

    テスト作成

--- examples/test.rs
+++ examples/test.rs
@@ -1,1 +1,54 @@
-fn main() {}
+use viewkit::platform::{
+    PlatformApplication,
+    PlatformEvent,
+    PlatformWindow,
+    WindowConfig,
+};
+use viewkit::platform::linux::LinuxBackend;
+
+struct ExampleApplication;
+
+impl PlatformApplication for ExampleApplication {
+    fn handle_event(
+        &mut self,
+        event: PlatformEvent,
+        _window: &dyn PlatformWindow,
+    ) {
+        match event {
+            PlatformEvent::Resumed { viewport } => {
+                println!("resumed: {viewport:?}");
+            }
+
+            PlatformEvent::Resized { viewport } => {
+                println!("resized: {viewport:?}");
+            }
+
+            PlatformEvent::RedrawRequested => {
+                println!("redraw requested");
+            }
+
+            PlatformEvent::CloseRequested => {
+                println!("close requested");
+            }
+
+            _ => {}
+        }
+    }
+}
+
+fn main() -> Result<
+    (),
+    Box<dyn std::error::Error>,
+> {
+    let backend = LinuxBackend::new(
+        ExampleApplication,
+        WindowConfig {
+            title: String::from("ViewKit Example"),
+            ..WindowConfig::default()
+        },
+    );
+
+    backend.run()?;
+
+    Ok(())
+}
