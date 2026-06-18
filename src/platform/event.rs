569015315de34da6592e004b4b82483bd3af046a
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-06-18 09:40:45 +0900

    マウスホイール入力イベントを定義

--- src/platform/event.rs
+++ src/platform/event.rs
@@ -7,18 +7,17 @@ pub enum PlatformEvent {
     Resumed {
         viewport: Viewport,
     },
-
     Resized {
         viewport: Viewport,
     },
-
     ScaleFactorChanged {
         viewport: Viewport,
     },
-
+    Scroll {
+        delta_x: f32,
+        delta_y: f32,
+    },
     Focused(bool),
-
     RedrawRequested,
-
     CloseRequested,
 }
