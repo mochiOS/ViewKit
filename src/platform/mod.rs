c142ea5f4fc11cddf70c35ac7a3703ac7281afc8
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-07-05 18:35:32 +0900

    mochios platformを追加

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -4,5 +4,8 @@ mod window;
 #[cfg(target_os = "linux")]
 pub mod linux;
 
+#[cfg(target_os = "mochios")]
+pub mod mochios;
+
 pub use event::{ButtonState, PlatformEvent, PointerButton};
 pub use window::{PlatformApplication, PlatformWindow, WindowConfig};
