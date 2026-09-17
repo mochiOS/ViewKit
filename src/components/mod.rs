26c302af55cf6264a111979668f287c324f291f6
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-09-17 18:36:14 +0900

    export standard app layout containers

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -44,6 +44,7 @@ mod vstack;
 mod zstack;
 
 pub use background::Background;
+pub use content_area::ContentArea;
 pub use dialog::Dialog;
 pub use divider::Divider;
 pub use form::{Form, FormSections};
@@ -74,6 +75,7 @@ pub use image::{Image, ImageContentMode};
 pub use list::{List, ListRow};
 pub use menu::{Menu, MenuItem};
 pub use message_bubble::{MessageBubble, MessageDirection};
+pub use navigation_split_view::NavigationSplitView;
 pub use navigation_split_view::NavigationSplitView;
 pub use picker::Picker;
 pub use popover::Popover;
